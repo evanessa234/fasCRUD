ec26b0a26ab5f0aefbb0423d6b91b1538ab668db
unknown <heenareenasingh234@gmail.com>
2020-08-01 12:05:23 -0700

    Revert "Update config.js"
    This reverts commit 9541d59456122aec554089c639f191b437b813cf.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
       user: 'root', // your database username
       password: '', // your database password
       port: 3306, // default MySQL port
-      db: 'fasdbexample1', // your database name
+      db: 'fasdbnew', // your database name
     },
     server: {
       host: '127.0.0.1',
